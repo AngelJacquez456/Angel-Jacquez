13640acd8fd9381202b224383d2bc7f9b4971535
AngelJacquez456 <166871073+AngelJacquez456@users.noreply.github.com>
2024-06-07 23:51:36 -0600

    Add files via upload

---
+++ Practica7/src/musica/Calimba.java
@@ -0,0 +1,11 @@
+package musica;
+
+public class Calimba extends InstrumentosMusicales{
+    public Calimba(){
+        tipodeinstrumento="Calimba";
+    }
+    public void tocarInstrumento(){
+        System.out.println("Tocar el Calmiba");
+    }
+    
+}
