1963f4a9b259df0e9c393b1348a157dd318d53c4
AngelJacquez456 <166871073+AngelJacquez456@users.noreply.github.com>
2024-06-18 05:32:22 -0600

    Delete Practice10 directory

--- Practice10/src/ejemploexepciones/Inicio.java
+++
@@ -1,9 +0,0 @@
-package ejemploexepciones;
-public class Inicio {
-void lanzaSiNegativo( int parametro ) throws Miexcepcion {
-if ( parametro < 0 )
-throw new Miexcepcion( "Num negativo" );
-}
-}
-
-
